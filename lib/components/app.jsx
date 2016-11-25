122f7f8946afeba87b4aef6cbbc0bd0c1f37f4c6
alexandersmanning <alexandersmanning@gmail.com>
2016-11-25 13:39:36 -0800

    Able to now display graph through react

--- lib/components/app.jsx
+++ lib/components/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ForceGraphContainer from './force_graph/force_graph_container';
 
-const App = () => {
-	<div className="main-container">
-	
-	</div>
+const App = () => { 
+	return (
+		<div className="main-container">
+			<header className="main-nav-bar">
+				<h1 className="header-logo">AirDnG</h1>
+			</header>
+			<ForceGraphContainer />
+		</div>
+	)
 };
 
 export default App;
